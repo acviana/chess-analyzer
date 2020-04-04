7d790eb58a97fbdd92044b19c8bd105c78647411
Alex C. Viana <alexcostaviana@gmail.com>
2020-04-04 17:35:48 -0500

    Added analysis report and underlaying functionality.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# chess-analyzer-
-Playing around with analyzing games, optimized for chess.com data.
+# chess-analyzer
+A small chess analysis library optimized for chess.com data.
+
+### Example Usage
+
+```
+$ python chess_analyzer/download.py
+Found 192 games from 2018-01 to 2020-04
+
+$ python chess_analyzer/analysis.py
+197 games found in data/*.pgn
+Total Games: 197
+Total Wins: 102 (52%)
+Total Losses: 95 (48%)
+Latest ELO: 649 (2020.04.01)
+Best Win: -126 - supersnorre (773) vs acviana (647) 0-1
+Total Opponents: 190
+Repeat Opponents: 7
+
+
+By time control:
+
+
+Time Control: 60s
+Total Games: 169
+Total Wins: 90 (53%)
+Total Losses: 79 (47%)
+Latest ELO: 649 (2020.04.01)
+Best Win: -101 - jogi221 (708) vs acviana (607) 0-1
+
+
+Time Control: 600s
+Total Games: 17
+Total Wins: 10 (59%)
+Total Losses: 7 (41%)
+Latest ELO: 1114 (2019.04.14)
+Best Win: -37 - ShabanAli (1151) vs acviana (1114) 0-1
+
+
+Time Control: 300s
+Total Games: 7
+Total Wins: 1 (14%)
+Total Losses: 6 (86%)
+Latest ELO: 924 (2019.07.04)
+Best Win: 61 - jessejoe521 (888) vs acviana (949) 0-1
+
+...
+```
--- chess_analyzer/analysis.py
+++ chess_analyzer/analysis.py
@@ -6,6 +6,47 @@ import pandas as pd
 from chess_analyzer.core import parse_game_file
 
 
+class AnalyzeGameSet:
+    def __init__(self, df, username):
+        self.df = df
+        self.username = username
+
+    @property
+    def best_win(self):
+        return self.df[self.df.is_win].sort_values("elo_spread").iloc[0]
+
+    @property
+    def game_count(self):
+        return len(self.df)
+
+    @property
+    def win_count(self):
+        return len(self.df[self.df.is_win])
+
+    @property
+    def last_elo(self):
+        return (
+            self.last_game.whiteelo
+            if self.last_game.is_white
+            else self.last_game.blackelo
+        )
+
+    @property
+    def last_game(self):
+        return self.df.sort_values("start_datetime").iloc[-1]
+
+    @property
+    def loss_count(self):
+        return len(self.df[~self.df.is_win])
+
+    @property
+    def opponent_list(self):
+        return (
+            self.df[self.df.white != self.username].white.values.tolist()
+            + self.df[self.df.black != self.username].black.values.tolist()
+        )
+
+
 def load_games(src):
     all_games = []
     filename_list = glob.glob(src)
@@ -16,13 +57,13 @@ def load_games(src):
 
 
 def enrich_game_dataframe(df, username):
-    df["color"] = ["white" if item.white == username else "black" for item in df.iloc]
+    df["color"] = ["white" if item == username else "black" for item in df.white]
     df["ranking"] = [
         item.whiteelo if item.white == username else item.blackelo for item in df.iloc
     ]
     df["ranking"] = df.ranking.astype(int)
     df["is_win"] = [True if username in item.termination else False for item in df.iloc]
-    df["termination_mode"] = [item.termination.split(" ")[-1] for item in df.iloc]
+    df["termination_mode"] = [item.split(" ")[-1] for item in df.termination.iloc]
     df["whiteelo"] = df.whiteelo.astype(int)
     df["blackelo"] = df.blackelo.astype(int)
     df["is_white"] = [item.white == username for item in df.iloc]
@@ -46,6 +87,26 @@ def enrich_game_dataframe(df, username):
     return df
 
 
+def print_win_loss_report(analyze_game_set_object):
+    print(f"Total Games: {analyze_game_set_object.game_count}")
+    print(
+        f"Total Wins: {analyze_game_set_object.win_count} "
+        f"({analyze_game_set_object.win_count / analyze_game_set_object.game_count * 100 :.0f}%)"
+    )
+    print(
+        f"Total Losses: {analyze_game_set_object.loss_count} "
+        f"({analyze_game_set_object.loss_count / analyze_game_set_object.game_count * 100 :.0f}%)"
+    )
+    last_game = analyze_game_set_object.last_game
+    print(f"Latest ELO: {analyze_game_set_object.last_elo}" f" ({last_game.date})")
+    best_win = analyze_game_set_object.best_win
+    print(
+        f"Best Win: {best_win.elo_spread} - {best_win.white} ({best_win.whiteelo}) vs "
+        f"{best_win.black} ({best_win.blackelo}) "
+        f"{best_win.result} "
+    )
+
+
 def main(src, username):
     raw_games = load_games(src)
     parsed_games = [parse_game_file(item) for item in raw_games]
@@ -56,6 +117,25 @@ def main(src, username):
 if __name__ == "__main__":
     src = "data/*.pgn"
     username = "acviana"
-    game_data = main(src=src, username=username)
-    print(f"{len(game_data)} games found in {src}")
-    print(game_data.loc[0])
+    game_dataframe = main(src=src, username=username)
+    print(f"{len(game_dataframe)} games found in {src}")
+
+    ags = AnalyzeGameSet(df=game_dataframe, username=username)
+    opponent_df = pd.DataFrame(ags.opponent_list)
+    repeat_opponent_df = opponent_df[0].value_counts()[
+        opponent_df[0].value_counts() > 1
+    ]
+    print_win_loss_report(ags)
+    print(f"Total Opponents: {len(set(ags.opponent_list))}")
+    print(f"Repeat Opponents: {len(repeat_opponent_df)}")
+    print("\n")
+
+    print(f"By time control: ")
+    for item in game_dataframe.timecontrol.value_counts().index:
+        print("\n")
+        print(f"Time Control: {item}s")
+        print_win_loss_report(
+            AnalyzeGameSet(
+                df=game_dataframe[game_dataframe.timecontrol == item], username=username
+            )
+        )
