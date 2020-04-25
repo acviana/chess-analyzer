d9fa86c441218bee5470093006cbe1e3cdcc03c7
Alex C. Viana <alexcostaviana@gmail.com>
2020-04-25 18:10:24 -0500

    Fixed fixture file path for test_split_bulk_file_download

--- test/test_download.py
+++ test/test_download.py
@@ -3,7 +3,7 @@ from chess_analyzer.download import split_bulk_file_download
 
 def test_split_bulk_file_download():
     # with open("test/test_pgn_game_set.txt", "rb") as f:
-    with open("test/test_test_test.txt", "r") as f:
+    with open("test/test_pgn_game_set.txt", "r") as f:
         test_data = f.read()
     test_result = split_bulk_file_download(test_data)
     assert len(test_result) == 3
